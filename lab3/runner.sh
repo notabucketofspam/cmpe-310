559a50d137468463a1e7b9bc8d3a49b1827f493d
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2026-09-19 13:05:20 -0400

    Update runner.sh

--- lab3/runner.sh
+++ lab3/runner.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/sh
 cd ..
 ./push.sh
-ssh brick_wg0 "cd /home/brick/cmsc-310/lab3&&source ./runme"
+ssh brick_wg0 "cd /home/brick/cmpe-310/lab3&&gcc -no-pie -nostdlib lab3.s -o lab3"
+echo "build ok"
+sleep 10
